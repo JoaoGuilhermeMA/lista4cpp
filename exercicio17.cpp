9da540f378d07c3f6b20aebe5b967a1433bf671e
JoaoGuilhermeMA <joomoreira36@gmail.com>
2023-05-17 18:34:54 -0300

    adicionando exercicio 17

---
+++ exercicio17.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+/*
+Elabore um algoritmo que leia um vetor ORIGINAL de 20 posições de inteiro, verifique e informe se os seus
+elementos estão em ordem crescente, ordem decrescente ou ordem aleatória.
+*/
+int main()
+{
+    int vetor[5], crescente = 0, decrescente = 0;
+
+    for (int i = 0; i < 5; i++)
+    {
+        cin >> vetor[i];
+        if (i > 0)
+        {
+            if (vetor[i] > vetor[i - 1])
+            {
+                crescente++;
+            }else{
+                decrescente++;
+            }
+        }
+    }
+
+    if(crescente == 4){
+        cout << "ordem crescente" << endl;
+    }else if(decrescente == 4){
+        cout << "ordem decrescente" << endl;
+    }else{
+        cout << "ordem aleatoria" << endl;
+    }
+    
+}
